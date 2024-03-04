43cac3fee0eae3a1d3f6c27f43a0d7ebcca9d107
Anusha <anusha@Anushas-MacBook.local>
2024-03-04 14:11:53 -0600

    Added vault codes with javascript and alerts

---
+++ main.js
@@ -0,0 +1,24 @@
+/* Pseudocode 
+1. Create an index.html file and main.js file in the blcok07 coursework folder.
+2. Open index.html in browser and link the main.js file to the index.html file.
+3. Create a string by using the alert message that says You have received this 
+message because you have been chosen to open an important vault. Here is the secret 
+combination:
+4. Assign three variables corresponding to three different arithmetic operators: +, -, * using const and naming each variable.
+5. Set each of those variables equal to either 10, 40, or 39 respectively. 
+6. Add comments to explain each step in the process.
+7. Create another alert box that displays the vault codes of 10, 40, and 39.
+*/
+
+//Creating a string for the user
+
+const addition = 4 + 6;
+console.log(addition);
+
+const difference = 50 - 10;
+console.log(difference);
+
+const product = 13 * 3;
+console.log(product);
+
+alert(`You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${addition} ${difference} ${product}`);
